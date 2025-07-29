b2fa7775c9958e62f087011f848cb69614a272dd
Carissa Traffie <carissa@traffie.com>
2025-07-29 13:34:07 -0700

    Initial Commit

---
+++ HealthPal/HealthPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPalApp.swift
+//  HealthPal
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
