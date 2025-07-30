83a0f4e8017f7dcf2ddca5ccdda563b941ceaf3b
Riya Chopade <riyachopade09@gmail.com>
2025-07-30 11:30:37 -0700

    tried things

--- MentalHealthView.swift
+++ MentalHealthView.swift
@@ -27,54 +27,53 @@ struct MentalHealthView: View {
                         .border(Color.black, width: 5)
                         .cornerRadius(10)
                 }
+                
+                    Text("Choose your emotion:")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                    HStack(){
+                        Button("😭") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                        Button("😢") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                        Button("☹️") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                        Button("😐") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                        Button("🙂") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                        Button("😁") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                        Button("😃") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        }
+                    }
+                    .padding(15)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    .padding(.top, 1.0)
+                    .cornerRadius(5)
                     
-                
-                Text("Choose your emotion:")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .padding(.top)
-                HStack(){
-                    Button("😭") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    Button("😢") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    Button("☹️") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    Button("😐") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    Button("🙂") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    Button("😁") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    Button("😃") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                }
-                .padding(8)
-                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                .padding(.top, 1.0)
-                
-                Text("Need a mental health break?")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .padding(.vertical)
-                
+                    Text("Need a mental health break?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                    
                     Text("Read a Book 📖")
                     Text("Meditate 🧘")
                     Text("Journal 📝")
+                    
+                    
+                    
                 Spacer()
+                }//vstack
                 
-                
-                
-            }//vstack
-            
-            
         }
     }
 }
