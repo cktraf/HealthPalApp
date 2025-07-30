8499edfc94204237eccd25e4d495f9fd6111abc7
Riya Chopade <riyachopade09@gmail.com>
2025-07-30 15:18:17 -0700

    made new file and tried to link

---
+++ PastMentalView.swift
@@ -0,0 +1,18 @@
+//
+//  PastMentalView.swift
+//  HealthPal
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import SwiftUI
+
+struct PastMentalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PastMentalView()
+}
