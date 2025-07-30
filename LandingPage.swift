6b07c03748dba0d43baf5cafae81086067c00600
Carissa Traffie <carissa@traffie.com>
2025-07-30 15:42:18 -0700

    Added background color to homepage

--- LandingPage.swift
+++ LandingPage.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 struct LandingPage: View {
     @State private var name : String = ""
-  var body: some View {
-    NavigationStack {
-      VStack {
-        Text("Welcome to Health Pal! What is your name?")
-          .font(.title2)
-          .multilineTextAlignment(.center)
-          TextField("Enter name:", text: $name)
-          .padding(.horizontal)
-          .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-          NavigationLink(destination: ContentView(name: $name)) {
-          Text("Go to app")
-        }
-      }
-      .padding()
-    }
-    .navigationBarBackButtonHidden(true)
+    var body: some View {
+        ZStack {
+            Color.lightBlue
+                .ignoresSafeArea(.all)
+            VStack {
+                Text("Welcome to Health Pal! What is your name?")
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                TextField("Enter name:", text: $name)
+                    .padding(.horizontal)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                NavigationLink(destination: ContentView(name: $name)) {
+                        Text("Go to app")
+                    }// nav link
+            }//v stack
+            .padding()
+            .navigationBarBackButtonHidden(true)
+      }//z stack
   }//body
 }
 
