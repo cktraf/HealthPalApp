c2575450c30ec3035d8597fd884d704c89ae3592
Carissa Traffie <carissa@traffie.com>
2025-07-31 09:47:17 -0700

    working on logo

--- LandingPage.swift
+++ LandingPage.swift
@@ -13,15 +13,22 @@ struct LandingPage: View {
             Color.lightBlue
                 .ignoresSafeArea(.all)
             VStack {
-                Text("Welcome to Health Pal! What is your name?")
+                Image("Logo")
+                    
+                Text("Welcome!")
                     .font(.title2)
                     .multilineTextAlignment(.center)
-                TextField("Enter name:", text: $name)
+                TextField("What is your name?", text: $name)
                     .padding(.horizontal)
                     .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
                 NavigationLink(destination: ContentView(name: $name)) {
-                        Text("Go to app")
+                    Text("Enter")
+                        .fontWeight(.medium)
                     }// nav link
+                .frame(width: 300, height: 50)
+                .background(Color.darkBlue)
+                .foregroundColor(.white)
+                .cornerRadius(20)
             }//v stack
             .padding()
             .navigationBarBackButtonHidden(true)
