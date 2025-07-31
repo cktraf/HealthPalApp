33bc4527cf9e4aea0d801abd9c294d4b44c41065
Carissa Traffie <carissa@traffie.com>
2025-07-31 10:13:12 -0700

    Final landing page, fixed link

--- LandingPage.swift
+++ LandingPage.swift
@@ -9,36 +9,38 @@ import SwiftUI
 struct LandingPage: View {
     @State private var name : String = ""
     var body: some View {
-        ZStack {
-            Color.lightBlue
-                .ignoresSafeArea(.all)
-            VStack {
-                Image("Logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                Text("Welcome!")
-                    .font(.title2)
-                    .fontWeight(.medium)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 70)
-                    .foregroundColor(Color(hue: 0.633, saturation: 0.86, brightness: 0.601))
-                TextField("What is your name?", text: $name)
-                    .frame(width: 325, height: 45)
-                    .padding(.horizontal)
-                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                NavigationLink(destination: ContentView(name: $name)) {
-                    Text("Enter")
+        NavigationStack {
+            ZStack {
+                Color.lightBlue
+                    .ignoresSafeArea(.all)
+                VStack {
+                    Image("Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Text("Welcome!")
+                        .font(.title2)
                         .fontWeight(.medium)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 70)
+                        .foregroundColor(Color(hue: 0.633, saturation: 0.86, brightness: 0.601))
+                    TextField("What is your name?", text: $name)
+                        .frame(width: 325, height: 45)
+                        .padding(.horizontal)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    NavigationLink(destination: ContentView(name: $name)) {
+                        Text("Enter")
+                            .fontWeight(.medium)
                     }// nav link
-                .frame(width: 300, height: 50)
-                .background(Color(hue: 0.633, saturation: 0.86, brightness: 0.601))
-                .foregroundColor(.white)
-                .cornerRadius(20)
+                    .frame(width: 300, height: 50)
+                    .background(Color(hue: 0.633, saturation: 0.86, brightness: 0.601))
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                    .padding()
+                }//v stack
                 .padding()
-            }//v stack
-            .padding()
-            .navigationBarBackButtonHidden(true)
-      }//z stack
+                .navigationBarBackButtonHidden(true)
+            }//z stack
+        }//nav stack
   }//body
 }
 
